598a66061d169a75d315af054337c3f649ed4745
IvanTepes <ivan.tepes@outlook.com>
2020-06-25 13:16:14 +0000

    Added WhatCanIDrink testing

---
+++ spec/calcSpec.js
@@ -0,0 +1,45 @@
+/* describe('Calculator', function () {
+
+
+    beforeEach(function() {
+        calc = new Calculator
+    });
+    
+
+    describe('Addition tests', function() {
+        it('should return 42', function() {
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
+        });
+        it("should return 26", function() {
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
+        });
+        it("should return an error if we don't supply two numbers", function() {
+            spyOn(window, "alert");
+            calc.add("Hitchhikers");
+            expect(window.alert).toHaveBeenCalledWith
+                ("Error!");
+        });
+    });
+}); */
+
+
+describe("My whatCanIDrink function", function() {
+    beforeEach(function() {
+        drink = new whatCanIDrink();
+    });
+
+    describe("Checks age", function() {
+        it("should exist", function() {
+            expect(whatCanIDrink).toBeDefined();
+        });
+
+        it("should return drink toddy when called as whatCanIDrink(13)", function() {
+            var result = whatCanIDrink(13)
+            expect(result).toBe("Drink Toddy");
+        });
+    })
+})
